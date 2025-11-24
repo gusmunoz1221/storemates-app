deced275781212531a5d4c7684c0579716923755
gustavo <gustavito12217@gmail.com>
2025-11-24 20:16:53 -0300

    commit inicial

---
+++ src/main/java/com/storemates/StorematesApplication.java
@@ -0,0 +1,13 @@
+package com.storemates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StorematesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StorematesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/storemates/StorematesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.storemates;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StorematesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
