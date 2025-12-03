eb06f579fb5d7563d3be19f7f667620013ead466
gustavo <gustavito12217@gmail.com>
2025-12-03 17:45:50 -0300

    feat(order): created orderservice

--- src/main/java/com/storemates/order/service/OrderService.java
+++ src/main/java/com/storemates/order/service/OrderService.java
@@ -1,5 +1,18 @@
 package com.storemates.order.service;
 
 
+import com.storemates.order.dto.OrderRequestDTO;
+import com.storemates.order.dto.OrderResponseDTO;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 public interface OrderService {
+    // CHECKOUT
+    OrderResponseDTO createOrder(OrderRequestDTO request);
+
+    // BUSCAR POR ID
+    OrderResponseDTO getOrderById(Long id);
+
+    // LISTAR (ADMIN)
+    Page<OrderResponseDTO> getAllOrders(Pageable pageable);
 }
